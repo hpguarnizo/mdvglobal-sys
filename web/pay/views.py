7427e80f7f98390f4ca2797d107a66e13a91aaa7
Gaston Zarate <gaston.z195@gmail.com>
2017-12-19 14:56:18 -0300

    Saco el modo sandbox

--- web/pay/views.py
+++ web/pay/views.py
@@ -140,7 +140,7 @@ def buy_my_premium(request):
     premium=Premium.objects.all().first()
     if request.POST.get('token',''):
         mp = mercadopago.MP(os.environ.get('ACCESS_TOKEN_MP'))
-        mp.sandbox_mode(True)
+        #mp.sandbox_mode(True)
 
         dic = {
             "transaction_amount": 1,
@@ -200,7 +200,7 @@ def buy_my_ministerial(request):
     ministerial=Ministerial.objects.all().first()
     if request.POST.get('token',''):
         mp = mercadopago.MP(os.environ.get('ACCESS_TOKEN_MP'))
-        mp.sandbox_mode(True)
+        #mp.sandbox_mode(True)
 
         dic = {
             "transaction_amount": 1,
@@ -343,14 +343,13 @@ def buy_my_donacion(request):
 def cancel_suscription(request):
     user = request.user
     mp = mercadopago.MP(os.environ.get('ACCESS_TOKEN_MP'))
-    cancel = mp.delete("/v1/customers/:%s/" %user.get_customer_id())
+    cancel = mp.delete("/v1/customers/%s/" %user.get_customer_id())
     json.dumps(cancel, indent=4)
-    assert False
-    if cancel['status']==201:
+    if cancel['status']==200:
         user.desuscribir()
         return HttpResponseRedirect(reverse('home_panel')+"?exito=true")
     else:
-        return HttpResponseRedirect(reverse('home_panel')+"?exito=true")
+        return HttpResponseRedirect(reverse('home_panel')+"?exito=false")
 
 
 @csrf_exempt
